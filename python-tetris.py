5a0ee0de7d4654c79a581deb9cf47c1f0b10baaf
Tai Lai <ctlai95@gmail.com>
2017-07-22 12:35:42 -0600

    WIP switching to laptop

--- python-tetris.py
+++ python-tetris.py
@@ -1,142 +1,30 @@
 import pyglet
 
-unit = 50
+UNIT = 40
 
-window = pyglet.window.Window(10*unit, 22*unit)
+window = pyglet.window.Window(10*UNIT, 22*UNIT)
 # window.push_handlers(pyglet.window.event.WindowEventLogger())
 
 global current_piece
 
 
-class point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+class piece:
+    def __init__(self, coords):
+        self.coords = []
+        for c in coords:
+            self.coords.append(tuple(x * UNIT for x in c))
+        self.size = len(coords)
 
-
-class square:
-    def __init__(self, origin, side_length):
-        self.origin = origin
-        self.side_length = side_length
-
-    def position(self):
-        return (self.origin.x, self.origin.y,
-                self.origin.x+self.side_length, self.origin.y,
-                self.origin.x+self.side_length, self.origin.y+self.side_length,
-                self.origin.x, self.origin.y+self.side_length)
-
-    def move_right(self):
-        self.origin.x += self.side_length
-
-    def move_left(self):
-        self.origin.x -= self.side_length
-
-    def move_up(self):
-        self.origin.y += self.side_length
-
-    def move_down(self):
-        self.origin.y -= self.side_length
-
-
-class tetromino:
-    def __init__(self, type, origin):
-        self.pieces = []
-        if type == "I":
-            for i in range(0, 4):
-                self.pieces.append(
-                    square(origin.x + (i * unit),
-                           origin.y,
-                           unit))
-
-        if type == "O":
-            for i in range(0, 2):
-                for y in range(0, 2):
-                    self.pieces.append(
-                        square(origin.x + (y * unit),
-                               origin.y + (i * unit),
-                               unit))
-
-        if type == "T":
-            for i in range(0, 3):
-                self.pieces.append(
-                    square(origin.x + (i * unit),
-                           origin.y,
-                           unit))
-            self.pieces.append(
-                square(origin.x + unit,
-                       origin.y + unit,
-                       unit)
+    def opengl_coords(self):
+        t = []
+        for c in self.coords:
+            t.append(
+                (c[0], c[1],
+                 c[0]+UNIT, c[1],
+                 c[0]+UNIT, c[1]+UNIT,
+                 c[0], c[1]+UNIT)
             )
-
-        if type == "S":
-            for i in range(0, 2):
-                self.pieces.append(
-                    square(origin.x + (i * unit),
-                           origin.y,
-                           unit)
-                )
-            for i in range(1, 3):
-                self.pieces.append(
-                    square(origin.x + (i * unit),
-                           origin.y + unit,
-                           unit)
-                )
-
-        if type == "Z":
-            for i in range(1, 3):
-                self.pieces.append(
-                    square(origin.x + (i * unit),
-                           origin.y,
-                           unit)
-                )
-            for i in range(0, 2):
-                self.pieces.append(
-                    square(origin.x + (i * unit),
-                           origin.y + unit,
-                           unit)
-                )
-
-        if type == "J":
-            for i in range(0, 3):
-                self.pieces.append(
-                    square(origin.x + (i * unit),
-                           origin.y,
-                           unit)
-                )
-            self.pieces.append(
-                square(origin.x,
-                       origin.y + unit,
-                       unit)
-            )
-
-        if type == "L":
-            for i in range(0, 3):
-                self.pieces.append(
-                    square(
-                        point(origin.x + (i * unit), origin.y),
-                        unit)
-                )
-            self.pieces.append(
-                square(
-                    point(origin.x + (2 * unit), origin.y + unit),
-                    unit)
-            )
-
-    def move_right(self):
-        for p in self.pieces:
-            p.move_right()
-
-    def move_left(self):
-        for p in self.pieces:
-            p.move_left()
-
-    def move_up(self):
-        for p in self.pieces:
-            p.move_up()
-
-    def move_down(self):
-        for p in self.pieces:
-            p.move_down()
+        return t
 
 
 @window.event
@@ -151,17 +39,20 @@ def on_text_motion(motion):
         current_piece.move_down()
 
 
-current_piece = tetromino("L", point(3*unit, 20*unit))
+current_piece = piece([(0, 0),
+                       (1, 0),
+                       (2, 0),
+                       (3, 0)])
 
 
 @window.event
 def on_draw():
     window.clear()
 
-    for c in current_piece.pieces:
+    for i in range(0, current_piece.size):
         pyglet.graphics.draw_indexed(4, pyglet.gl.GL_TRIANGLES,
                                      [0, 1, 2, 0, 2, 3],
-                                     ('v2i', c.position()))
+                                     ('v2i', current_piece.opengl_coords()[i]))
 
 
 pyglet.app.run()
