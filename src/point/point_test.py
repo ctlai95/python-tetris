06246974af2a312ad5a1041e1f17674bf48616cc
Tai Lai <ctlai95@gmail.com>
2017-12-13 19:31:22 -0700

    more comprehensive point test

--- src/point/point_test.py
+++ src/point/point_test.py
@@ -2,15 +2,21 @@ from src.point import point
 
 
 def test_x():
-    p = point.Point((1, 0))
-    assert p._x() == 1
+    for i in range(10):
+        for j in range(22):
+            p = point.Point((i, j))
+            assert p._x() == i
 
 
 def test_y():
-    p = point.Point((0, -2))
-    assert p._y() == -2
+    for i in range(10):
+        for j in range(22):
+            p = point.Point((i, j))
+            assert p._y() == j
 
 
 def test_xy():
-    p = point.Point((-3, 4))
-    assert p._xy() == (-3, 4)
+    for i in range(10):
+        for j in range(22):
+            p = point.Point((i, j))
+            assert p._xy() == (i, j)
