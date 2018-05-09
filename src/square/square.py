1d5e06b9bf88308991ec14124c0b822f8726ca7c
henrytran28 <henrytrann28@gmail.com>
2018-05-08 20:15:48 -0600

    remove unused arg

--- src/square/square.py
+++ src/square/square.py
@@ -33,10 +33,6 @@ class Square:
         self.y += y
 
     def render_square(self):
-        """Renders the square to the screen
-
-        Args:
-            color (int list): The color to render the square in [R, G, B]
-        """
+        """Renders the square to the screen"""
         r = Renderer(self.x, self.y, self.color)
         r.draw()
